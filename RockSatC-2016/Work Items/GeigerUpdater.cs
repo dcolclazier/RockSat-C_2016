fe454a308f4173f9c10a1c72d77a430973fcfd62
David Colclazier <david@jdc.tech>
2016-04-26 23:52:01 -0600

    geiger mayhem

--- RockSatC-2016/Work Items/GeigerUpdater.cs
+++ RockSatC-2016/Work Items/GeigerUpdater.cs
@@ -6,20 +6,35 @@ using RockSatC_2016.Event_Data;
 using RockSatC_2016.Utility;
 
 namespace RockSatC_2016.Work_Items {
+
+    
+
     public class GeigerUpdater : Action {
+
+        static InterruptPort geiger1;
+        //static InterruptPort geiger2;
+
         public GeigerUpdater(Cpu.Pin shieldedGeigerInterrupt, Cpu.Pin unshieldedGeigerInterrupt) {
 
+            //Debug.Print("Creating Interrupt Port for shielded geiger counter...");
+            //var shieldedGeiger = new InterruptPort(shieldedGeigerInterrupt,true,Port.ResistorMode.PullUp,
+            //    Port.InterruptMode.InterruptEdgeLevelHigh);
+            //Debug.Print("Created.");
+
             Debug.Print("Creating Interrupt Port for shielded geiger counter...");
-            var shieldedGeiger = new InterruptPort(shieldedGeigerInterrupt,true,Port.ResistorMode.PullUp,
-                Port.InterruptMode.InterruptEdgeLevelHigh);
+            geiger1 = new InterruptPort(shieldedGeigerInterrupt, true, Port.ResistorMode.PullUp,
+                Port.InterruptMode.InterruptEdgeLevelLow);
             Debug.Print("Created.");
+
+
             //Debug.Print("Creating Interrupt Port for unshielded geiger counter...");
             //var unshieldedGeiger = new InterruptPort(unshieldedGeigerInterrupt, true, Port.ResistorMode.PullUp,
             //    Port.InterruptMode.InterruptEdgeLevelLow);
             //Debug.Print("Created.");
 
             Debug.Print("Adding interrupt action for shielded geiger counter.");
-            shieldedGeiger.OnInterrupt += new NativeEventHandler(Shielded_Counter);
+            //shieldedGeiger.OnInterrupt += new NativeEventHandler(Shielded_Counter);
+            geiger1.OnInterrupt += new NativeEventHandler(Shielded_Counter);
             //Debug.Print("Adding interrupt action for unshielded geiger counter.");
             //unshieldedGeiger.OnInterrupt += Unshielded_Counter;
 
