86157b061756ca37e8b45674f0f9164a4ddb81e9
David Colclazier <david@jdc.tech>
2016-05-01 22:07:24 -0600

    More code cleanup

--- RockSatC-2016/Work Items/SerialBNOActions.cs
+++ RockSatC-2016/Work Items/SerialBNOActions.cs
@@ -1,8 +1,4 @@
-﻿using System.Threading;
-using Microsoft.SPOT;
-using Microsoft.SPOT.Hardware;
-using RockSatC_2016.Abstract;
-using RockSatC_2016.Event_Data;
+﻿using RockSatC_2016.Event_Data;
 using RockSatC_2016.Flight_Computer;
 using RockSatC_2016.Sensors;
 using RockSatC_2016.Utility;
@@ -15,19 +11,19 @@ namespace RockSatC_2016.Work_Items {
 
         private readonly BNOData _bnoData;
 
-        private readonly ThreadPool.WorkItem workItem;
+        private readonly ThreadPool.WorkItem _workItem;
 
         public SerialBNOActions() {
             _bnoSensor = new SerialBNO(SerialPorts.COM4,5000,5000,SerialBNO.Bno055OpMode.Operation_Mode_Ndof);
             _bnoData = new BNOData();
 
-            workItem = new ThreadPool.WorkItem(GyroUpdater, EventType.BNOUpdate, _bnoData, true);
+            _workItem = new ThreadPool.WorkItem(GyroUpdater, EventType.BNOUpdate, _bnoData, true);
 
             _bnoSensor.begin();
         }
 
         private void GyroUpdater() {
-
+            
             var gyro_vec = _bnoSensor.read_vector(SerialBNO.Bno055VectorType.Vector_Gyroscope);
             var accel_vec = _bnoSensor.read_vector(SerialBNO.Bno055VectorType.Vector_Accelerometer);
             _bnoData.temp = _bnoSensor.read_signed_byte(SerialBNO.Bno055Registers.Bno055_Temp_Addr);
@@ -47,38 +43,12 @@ namespace RockSatC_2016.Work_Items {
         }
 
         public void Start() {
-            FlightComputer.Instance.Execute(workItem);
+            _workItem.SetRepeat(true);
+            FlightComputer.Instance.Execute(_workItem);
         }
 
         public void Stop() {
-            throw new System.NotImplementedException();
+            _workItem.SetRepeat(false);
         }
     }
-
-    //public class BNOUpdater100Hz : Action {
-
-    //    private readonly Bno055 _bnoSensor;
-    //    private readonly BNOGyroData _bnoGyroData;
-    //    private readonly I2CDevice.Configuration _bnoConfig;
-    //    public BNOUpdater100Hz() {
-    //        _bnoConfig = new I2CDevice.Configuration(0x28, 100);
-    //        _bnoSensor = new Bno055(_bnoConfig);
-    //        _bnoGyroData = new BNOGyroData();
-    //        workItem = new ThreadPool.WorkItem(GryoUpdater, EventType.BNOUpdate100Hz, null, true);
-    //        FlightComputer.Instance.Execute(workItem);
-    //    }
-
-    //    private void GryoUpdater() {
-
-
-    //        _bnoGyroData.gyro_x = I2CBus.Instance().read16(_bnoConfig, 0x14, 0x15);
-    //        _bnoGyroData.gyro_y = I2CBus.Instance().read16(_bnoConfig, 0x16, 0x17);
-    //        _bnoGyroData.gyro_z = I2CBus.Instance().read16(_bnoConfig, 0x18, 0x19);
-    //        _bnoGyroData.accel_x = I2CBus.Instance().read16(_bnoConfig, 0x28, 0x29);
-    //        _bnoGyroData.accel_y = I2CBus.Instance().read16(_bnoConfig, 0x2A, 0x2B);
-    //        _bnoGyroData.accel_z = I2CBus.Instance().read16(_bnoConfig, 0x2C, 0x2D);
-
-    //        Thread.Sleep(10);
-    //    }
-    //}
 }
